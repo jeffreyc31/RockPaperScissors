88b89165522ead0d91be98fa94e4885a2372a775
jeffreyc31 <jeffcgit31@gmail.com>
2022-02-20 16:08:56 -0800

    Code game function

--- javascript/script.js
+++ javascript/script.js
@@ -16,6 +16,7 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     let result;
+    let tracker;
     computerSelection = computerPlay();
     /*console.log(computerSelection);*/
     playerSelection = prompt("Do you choose rock, paper, or scissors?");
@@ -23,28 +24,35 @@ function playRound(playerSelection, computerSelection) {
     /*console.log(playerSelection);*/
 
     if (playerSelection === computerSelection) {
+        tracker = 0;
         result = "It's a tie!"
     } else {
         switch (playerSelection) {
             case "rock":
                 if (computerSelection === "paper") {
                     result = "You lose! Paper beats rock."
+                    tracker = 2;
                 } else {
                     result = "You win! Rock beats scissors."
+                    tracker = 1;
                 }
                 break;
             case "paper":
                 if (computerSelection === "scissors") {
                     result = "You lose! Scissors beats paper."
+                    tracker = 2;
                 } else {
                     result = "You win! Paper beats rock."
+                    tracker = 1;
                 }
                 break;
             case "scissors":
                 if (computerSelection === "rock") {
                     result = "You lose! Rock beats scissors."
+                    tracker = 2;
                 } else {
                     result = "You win! Scissors beats paper."
+                    tracker = 1;
                 }
                 break;
         }
@@ -52,3 +60,31 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 /*console.log(playRound());*/
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    const winMessage = "You win!";
+    const loseMessage = "You lose";
+    for (let i = 0; i < 5; i++) {
+        let message = playRound();
+        console.log(message);
+        let resultPrint = message.slice(0, 8);
+        console.log(resultPrint);
+
+        if (resultPrint === winMessage) {
+            playerScore++;
+        } else if (resultPrint === loseMessage) {
+            computerScore++;
+        }
+
+        console.log(`Your score is ${playerScore} so far. The computer's score is ${computerScore}.`)
+    }
+    if (playerScore > computerScore) {
+        console.log(`The final score is ${playerScore}-${computerScore}. You won the tournament!`)
+    } else if (playerScore < computerScore) {
+        console.log(`The final score is ${playerScore}-${computerScore}. You lose the tournament!`)
+    } else {
+        console.log(`The final score is ${playerScore}-${computerScore}. You tied in the tournament!`)
+    }
+}
