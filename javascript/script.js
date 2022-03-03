d2de956d8a4d536d8b0ba6f1efb38daa29449fd0
jeffreyc31 <jeffcgit31@gmail.com>
2022-03-03 11:21:47 -0800

    Add scoreboard feature to html Delete unused variable 'tracker' from playRound

--- javascript/script.js
+++ javascript/script.js
@@ -16,41 +16,38 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     let result;
-    let tracker;
     computerSelection = computerPlay();
     //playerSelection = prompt("Do you choose rock, paper, or scissors?");
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        tracker = 0;
         result = "It's a tie!"
     } else {
         switch (playerSelection) {
             case "rock":
                 if (computerSelection === "paper") {
                     result = "You lose! Paper beats rock."
-                    tracker = 2;
                 } else {
                     result = "You win! Rock beats scissors."
-                    tracker = 1;
+
                 }
                 break;
             case "paper":
                 if (computerSelection === "scissors") {
                     result = "You lose! Scissors beats paper."
-                    tracker = 2;
+
                 } else {
                     result = "You win! Paper beats rock."
-                    tracker = 1;
+
                 }
                 break;
             case "scissors":
                 if (computerSelection === "rock") {
                     result = "You lose! Rock beats scissors."
-                    tracker = 2;
+
                 } else {
                     result = "You win! Scissors beats paper."
-                    tracker = 1;
+
                 }
                 break;
         }
